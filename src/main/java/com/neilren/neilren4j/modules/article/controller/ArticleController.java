f6fae58b2374152cc4afb6ccc6ef211ec45ff9d1
NeilRen <Mail@NeilRen.Com>
2017-06-20 14:12:24 +0800

    完成 文章评分功能

--- src/main/java/com/neilren/neilren4j/modules/article/controller/ArticleController.java
+++ src/main/java/com/neilren/neilren4j/modules/article/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.neilren.neilren4j.modules.article.controller;
 
 import com.neilren.neilren4j.common.controller.BaseController;
 
+import com.neilren.neilren4j.modules.article.entity.Article;
 import com.neilren.neilren4j.modules.article.entity.ArticleWithBLOBs;
 import com.neilren.neilren4j.modules.article.service.ArticleService;
 import com.neilren.neilren4j.modules.article.service.TagService;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -23,6 +26,12 @@ public class ArticleController extends BaseController {
     @Autowired
     private TagService tagService;
 
+    /**
+     * 根据文章ID获取文章
+     *
+     * @param ID 文章ID
+     * @return
+     */
     @RequestMapping("/Article/{ID}")
     public ModelAndView getArticleById(@PathVariable String ID) {
         ModelAndView mv = new ModelAndView();
@@ -34,5 +43,17 @@ public class ArticleController extends BaseController {
         mv.addObject("articleTop10ByViewsList", articleService.getArticleTop10ByViewsList());
         mv.setViewName("article/articlepage");
         return mv;
+    }
+
+    /**
+     * 给文章评分
+     *
+     * @param longId 文章ID
+     * @param grade  评分等级
+     */
+    @ResponseBody
+    @RequestMapping(value = "/Article/Grade", method = RequestMethod.POST)
+    public void setArticleGrade(String longId, String grade) {
+        articleService.setArticleGrade(longId, grade);
     }
 }

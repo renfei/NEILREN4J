105c4c6628eed96091b0054ec9b02c81d116817b
neil-ren <mail@neilren.com>
2018-07-16 13:00:51 +0800

    Remove old files

--- src/main/java/com/neilren/neilren4j/common/entity/Changefreq.java
+++
@@ -1,8 +0,0 @@
-package com.neilren.neilren4j.common.entity;
-
-/**
- * Created by neil on 29/06/2017.
- */
-public enum Changefreq {
-    always, hourly, daily, weekly, monthly, yearly
-}
--- src/main/java/com/neilren/neilren4j/common/persistence/BaseEntity.java
+++
@@ -1,9 +0,0 @@
-package com.neilren.neilren4j.common.persistence;
-
-import java.io.Serializable;
-
-/**
- * Created by neil on 07/06/2017.
- */
-public abstract class BaseEntity<T> implements Serializable {
-}
